eee5ff667f51ba4b7c15118cb6803a477ba0c9b8
GamJia <mgamga3@skuniv.ac.kr>
2023-05-18 18:51:16 +0900

    [Fix] Player Rig

---
+++ Assets/3.Script/ETC/RotateCursor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCursor : MonoBehaviour
+{
+    public Transform target;
+
+    private void Update()
+    {
+        transform.LookAt(target);
+        transform.eulerAngles += new Vector3(90, 0, 0);
+    }
+}
