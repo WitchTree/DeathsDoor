c034aa46c5234a735d6e6a13dbe0efb9abe86439
GamJia <mgamga3@skuniv.ac.kr>
2023-05-23 01:37:43 +0900

    [Create] Scene Ceramic Manor

---
+++ Assets/3.Script/ETC/CameraOrthographic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraOrthographic : MonoBehaviour
+{
+    public CinemachineVirtualCamera vcam; 
+    void Start()
+    {        
+        vcam.m_Lens.OrthographicSize = 3;
+    }
+
+}
+
+
+
+
+
+
