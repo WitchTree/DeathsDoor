962d3723e0bc047c8f4790fbde3d4dac3ba81ad7
rahyun <rravely@sookmyung.ac.kr>
2023-05-30 16:54:17 +0900

    [Create] 03 Scene R_Right

---
+++ Assets/3.Script/Enemy/POT_Mimic_Melee/SpinColliderDMG.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinColliderDMG : MonoBehaviour
+{
+    //플레이어 클래스
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //플레이어 닳기
+    }
+}
