0f443c50dcd8e706f8e2dd5e1f49232c3f9a4d67
reasonLee <dbfl6094@naver.com>
2023-05-31 10:11:11 +0900

    [Creat] 플레이어 추가
    최종 플레이어 추가

--- Assets/3.Script/Player/Sword/Sword_Data.cs
+++ Assets/3.Script/Player/Sword/Sword_Data.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObject/Sword_data")]
 public class Sword_Data : ScriptableObject
 {
-    public float damage = 100f;//공격력
+    public float damage = 1f;//공격력
 
-    public float DelayTime = 1.5f;//공격속도(지연시간?)
+    public float DelayTime = 0.5f;//공격속도(지연시간?)
 }
