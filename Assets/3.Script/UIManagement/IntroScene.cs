6a0488712c10df1506dc390defcfe3d460cf0e6a
GamJia <mgamga3@skuniv.ac.kr>
2023-06-02 18:28:53 +0900

    [Create] Intro Animation5

--- Assets/3.Script/UIManagement/IntroScene.cs
+++ Assets/3.Script/UIManagement/IntroScene.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class IntroScene : MonoBehaviour
 {
@@ -9,17 +10,31 @@ public class IntroScene : MonoBehaviour
     public Vector3 firstPoint;
     public Vector3 secondPoint;
     public Vector3 thirdPoint;
+    public Vector3 targetPoint;
+
     public GameObject firstPointObject;
     public GameObject secondPointObject;
     public GameObject thirdPointObject;
+    public GameObject targetObject;
+
     public bool isEntered;
     public bool isCorner;
     public bool isFinish;
     public bool isEnd;
+    public bool isLookat;
+    private bool talking = false;
+    private bool isStart = false;
+
     public Animator playerAnimator;
     public PlayerInput playerInput;
     public PlayerController playerController;
     public CinemachineVirtualCamera vcam;
+    public GameObject DialogueUI;
+    public Text Txt_Dialogue;
+    public string[] Dialogue;
+    private int index;
+    public float wordSpeed;
+
 
 
 
@@ -30,6 +45,7 @@ public class IntroScene : MonoBehaviour
         firstPoint =firstPointObject.transform.position;
         secondPoint=secondPointObject.transform.position;
         thirdPoint = thirdPointObject.transform.position;
+        targetPoint=targetObject.transform.position;
     }
 
     private void Update()
@@ -52,9 +68,9 @@ public class IntroScene : MonoBehaviour
             playerAnimator.SetBool("Run",true);
 
             if (player.gameObject.transform.position == secondPoint)
-            {
+            {                
                 FinishMove();
-                
+                Invoke(nameof(StartDialogue), 0.4f);
             }
            
         }
@@ -67,8 +83,8 @@ public class IntroScene : MonoBehaviour
 
             if (player.gameObject.transform.position == thirdPoint)
             {
-                ResetMove();
-               
+                ResetMove();                
+
             }
                        
         }
@@ -76,6 +92,28 @@ public class IntroScene : MonoBehaviour
         if (isEnd == true)
         {
             playerAnimator.SetBool("Run", false);
+        }
+
+        if(isLookat==true)
+        {
+            player.gameObject.transform.LookAt(targetPoint);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)&& isStart)
+        {
+            if (index < Dialogue.Length - 1)
+            {
+                index++;
+                Txt_Dialogue.text = "";
+                StartCoroutine(Typing());
+            }
+
+            else
+            {
+                zeroText();
+                DialogueUI.SetActive(false);
+                talking = false;                
+            }
         }
 
 
@@ -86,9 +124,12 @@ public class IntroScene : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isEntered=true;
+            isLookat = true;
             playerInput.isLock = true;
+            isStart = true;
             StartCoroutine("CameraZoomIn");
             StartCoroutine("CameraZoomIn1");
+            StartCoroutine("CameraZoomIn2");
 
         }        
     }
@@ -105,21 +146,22 @@ public class IntroScene : MonoBehaviour
     private void FinishMove()
     {
         isCorner = false;
-        isFinish = true;                
+        isFinish = true;       
+        
     }
 
     private void ResetMove()
     {
         isFinish = false;
-        isEnd = true;
+        isEnd = true;        
     }
 
     public IEnumerator CameraZoomIn()
     {
-        for (float f= 3.7f; f >= 2.4f; f -= 0.01f)
+        for (float f= 3.7f; f >= 3f; f -= 0.01f)
         {
             vcam.m_Lens.OrthographicSize =f;
-            yield return new WaitForSeconds(0.005f);
+            yield return new WaitForSeconds(0.05f);
         }
 
     }
@@ -129,15 +171,73 @@ public class IntroScene : MonoBehaviour
 
         var transposer = vcam.GetCinemachineComponent<CinemachineComposer>();
 
-        for (float f = 1; f <=2.14f; f += 0.02f)
+        for (float f = 1; f <= 2.71f; f += 0.02f)
         {
             transposer.m_TrackedObjectOffset.y = f;
-            yield return new WaitForSeconds(0.005f);
+            yield return new WaitForSeconds(0.01f);
         }
 
     }
 
-    
+    public IEnumerator CameraZoomIn2()
+    {
+
+        var transposer = vcam.GetCinemachineComponent<CinemachineComposer>();
+
+        for (float f = 0f; f >= -1.55f; f -= 0.01f)
+        {
+            transposer.m_TrackedObjectOffset.x = f;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+    }
+
+    public void NextLine()
+    {
+
+        //num++;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (index < Dialogue.Length - 1)
+            {
+                index++;
+                Txt_Dialogue.text = "";
+                StartCoroutine(Typing());
+            }
+
+            else
+            {
+                zeroText();
+                DialogueUI.SetActive(false);
+                talking = false;                
+            }
+        }
+
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in Dialogue[index].ToCharArray())
+        {
+            Txt_Dialogue.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void zeroText() 
+    {
+        Txt_Dialogue.text = "";
+        index = 0;        
+    }
+
+
+    private void StartDialogue()
+    {
+        DialogueUI.SetActive(true);
+        StartCoroutine(Typing());
+    }
+
+
 
 
 
